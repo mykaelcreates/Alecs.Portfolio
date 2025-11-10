8209f4e6a19fcd4557499bf360f3c7ff97c2be8f
Alec Mendoza <alec30@ksu.edu>
2025-11-10 17:04:58 -0600

    Bugs

---
+++ app.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSidebar() {
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+}
+
+function toggleSubMenu(button) {
+    button.classList.toggle('rotate');
+}
